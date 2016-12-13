f0fb8fec81bb5826008af86fe671d8559e7695fe
arnaud lustrat <arnaud lustrat>
2016-12-13 15:42:03 +0100

    static files3

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import static
+from . import settings
 
 urlpatterns = [
     url(r'', include('ommm.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
