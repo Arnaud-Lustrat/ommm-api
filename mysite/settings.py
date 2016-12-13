cc97c0328cdf971372a9e04f37daf05b844c28e5
arnaud lustrat <arnaud lustrat>
2016-12-13 16:54:09 +0100

    static files10

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,15 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = './static'
 MEDIA_ROOT = './media'
-
-STATICFILES_DIRS = (
-    './static',
-)
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_URL = '/static/'
