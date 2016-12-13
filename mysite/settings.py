6564c3875f06bfc50b353132fb51bbfb13ddc123
arnaud lustrat <arnaud lustrat>
2016-12-13 15:34:58 +0100

    static files2

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,17 +146,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
-MEDIA_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
-)
+#MEDIA_DIRS = (
+#    os.path.join(BASE_DIR, 'media'),
+#)
 
 # Rest configuration
 
