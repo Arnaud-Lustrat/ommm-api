0a67f05b19ad1bc0568fc0c3fb4a760eedbf25bf
arnaud lustrat <arnaud lustrat>
2016-12-13 17:35:40 +0100

    static files15

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,7 @@
-#from django.conf.urls import url, include
 from django.conf.urls import *
 from django.contrib.staticfiles.urls import static
 from . import settings
 
 urlpatterns = [
     url(r'', include('ommm.urls')),
-] # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#urlpatterns += patterns('',
-#    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-#)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
